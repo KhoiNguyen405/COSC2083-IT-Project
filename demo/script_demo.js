33e5ff815254f7e0e4bfc3f590372c9c3720dcf1
KhoiNguyen405 <s4052683@rmit.edu.vn>
2024-01-08 10:34:39 +0700

    Added comments for each function in js script file.

--- demo/script_demo.js
+++ demo/script_demo.js
@@ -1,11 +1,26 @@
+/**
+ * Function to allow a draggable element to be dropped in the given element.
+ * 
+ * @param {Event} ev: current ondragover event.
+ */
 function allowDrop(ev) {
     ev.preventDefault();
 }
-  
+
+/**
+ * Function to drag a draggable element.
+ * 
+ * @param {Event} ev: current drag event.
+ */
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
-  
+
+/**
+ * Function to drop dragged element into a dropzone container.
+ * 
+ * @param {Event} ev: current drop event.
+ */
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
@@ -20,10 +35,18 @@ function drop(ev) {
     return;
 }
 
+/**
+ * Check correctness of current attempt at activity:
+ *    - Count score of correct answers;
+ *    - Change border colour of answer box to green if correct, red if incorrect;
+ *    - Display score of current attemp.
+ * 
+ * @param {Node} ele: element that called this function.
+ */
 function checkAnswer(ele) {
     var score = 0;
     var activity_num = ele.parentNode.id;
-    var numQ = document.getElementById(activity_num).getAttribute("data-numq");
+    var numQ = Number(document.getElementById(activity_num).getAttribute("data-numq"));
     let dropzone_ID;
     let child_el;
 
@@ -45,6 +68,12 @@ function checkAnswer(ele) {
     displayScore(score, numQ);
 }
 
+/**
+ * Display the score of the current attempt at activity.
+ * 
+ * @param {Number} score: score of current attempt.
+ * @param {Number} numQ: max possible score of activity.
+ */
 function displayScore(score, numQ) {
     document.getElementById("score").style.display='block';
     document.getElementById("score").innerHTML = "Score: " + String(score) + "/" + String(numQ);
@@ -55,17 +84,35 @@ function displayScore(score, numQ) {
     }
 }
 
+/**
+ * Change border colour of answer box to green for correct answer.
+ * 
+ * @param {String} divID: id attribute of answer box.
+ */
 function gradeCorrect(divID) {
     document.getElementById(divID).style.border = "2px solid green";
 }
 
+/**
+ * Change border colour of answer box to red for incorrect answer.
+ * 
+ * @param {String} divID: id attribute of answer box.
+ */
 function gradeIncorrect(divID) {
     document.getElementById(divID).style.border = "2px solid red";
 }
 
+/**
+ * Reset activity area to default display:
+ *    - Move any draggable element in answer boxes back to starting container;
+ *    - Change border colour of answer boxes back to default grey;
+ *    - Hide score box.
+ * 
+ * @param {Node} ele: element that called this function.
+ */
 function clearDropArea(ele) {
     var activity_num = ele.parentNode.id;
-    var numQ = document.getElementById(activity_num).getAttribute("data-numq");
+    var numQ = Number(document.getElementById(activity_num).getAttribute("data-numq"));
 
     // Clear dropzones
     let dropzone_ID;
